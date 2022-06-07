dc7062d7a5ce14d314de7b24b7d36be7ddae3c68
Kacper Jajuga <kacper.jajuga123@gmail.com>
2022-06-08 01:20:59 +0200

    Create struct and dynamic array

--- Projekt2/Projekt2/Projekt2.cpp
+++ Projekt2/Projekt2/Projekt2.cpp
@@ -1,6 +1,25 @@
 ﻿#include <iostream>
 
+using namespace std;
+
+#define rozmiar 5
+
+struct STUDENT {
+    string imie;
+    string nazwisko;
+    int oceny[rozmiar];
+    double srednia_ocen;
+};
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    int N;
+    cout << "Podaj liczbe studentow: ";
+    cin >> N;
+    while (N < 1) {
+        cout << "Liczba studentow nie moze byc mniejsza od 1!" << endl;
+        cout << "Podaj poprawna liczbe studentow: ";
+        cin >> N;
+    }
+    STUDENT * tablica = new STUDENT[N];
 }
