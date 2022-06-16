9a6ea5648af125a57e2899a502e29fb2bd0f938d
Kacper Jajuga <kacper.jajuga123@gmail.com>
2022-06-16 18:59:13 +0200

    Add basic functions
    Add reading names from file, draw grades and find mean

--- Projekt2/Projekt2/Projekt2.cpp
+++ Projekt2/Projekt2/Projekt2.cpp
@@ -1,4 +1,7 @@
 ﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
 
 using namespace std;
 
@@ -7,19 +10,66 @@ using namespace std;
 struct STUDENT {
     string imie;
     string nazwisko;
-    int oceny[rozmiar];
+    double oceny[rozmiar];
     double srednia_ocen;
 };
 
+void pobierzIloscStudentow(int &ilosc);
+void pobierzStudentow(STUDENT tab[], int ilosc);
+void losowanieOcen(STUDENT tab[], int ilosc);
+void sredniaStudenta(STUDENT tab[], int ilosc);
+
 int main()
 {
+    srand(time(NULL));
     int N;
+    pobierzIloscStudentow(N);
+    STUDENT* tablica = new STUDENT[N];
+    pobierzStudentow(tablica, N);
+    losowanieOcen(tablica, N);
+    sredniaStudenta(tablica, N);
+}
+
+void pobierzIloscStudentow(int &ilosc) {
     cout << "Podaj liczbe studentow: ";
-    cin >> N;
-    while (N < 1) {
+    cin >> ilosc;
+    while (ilosc < 1) {
         cout << "Liczba studentow nie moze byc mniejsza od 1!" << endl;
         cout << "Podaj poprawna liczbe studentow: ";
-        cin >> N;
+        cin >> ilosc;
     }
-    STUDENT * tablica = new STUDENT[N];
+}
+
+void pobierzStudentow(STUDENT tab[], int ilosc) {
+    ifstream plik;
+    plik.open("Studenci.txt");
+    if (plik.is_open()) {
+        for (int i = 0; i < ilosc; i++) {
+            plik >> tab[i].imie >> tab[i].nazwisko;
+        }
+    }
+    else {
+        cout << "Blad otwierania pliku, sprawdz czy plik znajduje sie w tym samym folderze co program!" << endl;
+    }
+    plik.close();
+}
+
+void losowanieOcen(STUDENT tab[], int ilosc) {
+    double tab_oceny[7] = {2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5};
+    for (int i = 0; i < ilosc; i++) {
+        for (int j = 0; j < rozmiar; j++) {
+            tab[i].oceny[j] = tab_oceny[rand() % 7];
+        }
+    }
+}
+
+void sredniaStudenta(STUDENT tab[], int ilosc) {
+    double suma;
+    for (int i = 0; i < ilosc; i++) {
+        suma = 0;
+        for (int j = 0; j < rozmiar; j++) {
+            suma += tab[i].oceny[j];
+        }
+        tab[i].srednia_ocen = suma / rozmiar;
+    }
 }
